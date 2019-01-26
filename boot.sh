e4f1bbaec17392a7de2fb3857d10509f50b527ca
Eric Yang <ericyang879@gmail.com>
2019-01-26 13:14:49 -0500

    move `-d` parameter to front.

--- boot.sh
+++ boot.sh
@@ -9,9 +9,9 @@ elif [ "$1" == "nodejs" ]; then
 elif [ "$1" == "elastic" ]; then
     docker-compose run --rm --service-ports kibana
 elif [ "$1" == "db" ]; then
-    docker-compose run --rm --service-ports mongo -d
-    docker-compose run --rm --service-ports redis -d
-    docker-compose run --rm --service-ports mysql -d
+    docker-compose run -d --rm --service-ports mongo
+    docker-compose run -d --rm --service-ports redis
+    docker-compose run -d --rm --service-ports mysql
     echo "Mysql, MongoDB, and MongoDB are runing in daemon mode."
 else
     echo "Invalid Parameter -> $1, Only support nginx|python|nodejs|elastic"
